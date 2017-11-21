185a42e810712d9fd76dee9f3827f94449b3f0dd
Ahmed Ali <amali@qf.org.qa>
2017-11-21 10:21:57 -0500

    get only aljazeera

--- run.sh
+++ run.sh
@@ -1,14 +1,28 @@
 #!/bin/bash -ex 
 
-# this script expects story.txt to be exist in the same folder 
+# this script will make story from aljazeera website and SUMMA API's
 
-rm -fr data output.txt
+rm -fr data output.txt data_aljazeera
+
+#process aljazeera data 
+python scripts/getAljazeeraStory.py   > tmp$$
+cat tmp$$ | awk -F "\t" '{print $1}' > story.txt 
+cat tmp$$ | awk -F "\t" '{print $2}' > story_images.txt 
+rm -fr tmp$$
+data=data_aljazeera
+mkdir -p $data
 nl story.txt | while read id txt2; do
-     mkdir -p data
      txt=$(echo $txt2 | tr '\r' ' ' | sed 's: $::')
      echo "Processing: TEXT ${txt}"
      echo "Processing: ID ${id}"
-     python scripts/google_image_search.py -n 5 "$txt" data/$id
-     python scripts/tts.py "$txt" "data/$id"
+     python scripts/google_image_search.py -n 5 "$txt" $data/$id
+     python scripts/tts.py "$txt" "$data/$id"
 done
+
+nl story_images.txt | grep [0-9] |  while read id image; do
+    echo "Getting image: $id $image"
+    wget $image -O $data/$id/0.jpg
+done
+    
+
 rm -fr output.txt
