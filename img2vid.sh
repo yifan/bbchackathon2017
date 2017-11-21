ecaab2b64228d6cbee656c167cd5be9976ed3044
Yifan Zhang <freqyifan@gmail.com>
2017-11-21 17:07:23 +0000

    takes images.txt instead

--- img2vid.sh
+++ img2vid.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 touch list.txt
-for IMGFILE in $@; do
+cat images.txt | while read IMGFILE; do
   VIDFILE=${IMGFILE%\.*}.mp4
+  WAVFILE=$(dirname $IMGFILE).wav
   VIDFILE=mp4_aljazeera${VIDFILE##data_aljazeera}
   DIRNAME=$(dirname $VIDFILE)
   mkdir -p $DIRNAME
   ffmpeg -loop 1 -t 1 -i $IMGFILE -s hd720 -c:v mpeg4 -pix_fmt yuv420p -vf scale=1280:720 $VIDFILE
-  echo file $VIDFILE >> list.txt
+  if [[ -s $VIDFILE ]]; then
+    echo file \'$VIDFILE\' >> list.txt
+  else
+    echo ffmpeg -loop 1 -t 1 -i $IMGFILE -s hd720 -c:v mpeg4 -pix_fmt yuv420p -vf scale=1280:720 $VIDFILE >> failed.txt
+  fi
 done
 
-ffmpeg -f concat -safe 0 -i list.txt -c copy video.mp4
+ffmpeg -f concat -i list.txt -c copy video.mp4
 rm list.txt
