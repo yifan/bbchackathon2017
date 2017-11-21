41a02b5ee93e300f06a60c6a59d5e4dff4ab2d0a
Yifan Zhang <freqyifan@gmail.com>
2017-11-21 16:34:04 +0000

    produce video from image

--- img2vid.sh
+++ img2vid.sh
@@ -6,8 +6,8 @@ for IMGFILE in $@; do
   VIDFILE=mp4_aljazeera${VIDFILE##data_aljazeera}
   DIRNAME=$(dirname $VIDFILE)
   mkdir -p $DIRNAME
-  ffmpeg -loop 1 -i $IMGFILE -s hd720 -c:v mpeg4 -pix_fmt yuv420p $VIDFILE
-  echo $VIDFILE >> list.txt
+  ffmpeg -loop 1 -t 1 -i $IMGFILE -s hd720 -c:v mpeg4 -pix_fmt yuv420p -vf scale=1280:720 $VIDFILE
+  echo file $VIDFILE >> list.txt
 done
 
 ffmpeg -f concat -safe 0 -i list.txt -c copy video.mp4
