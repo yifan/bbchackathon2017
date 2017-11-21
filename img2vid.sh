2f7f69a35852b885a65a7a439e62762fa411f135
Yifan Zhang <freqyifan@gmail.com>
2017-11-21 16:19:01 +0000

    merge clips into single video

--- img2vid.sh
+++ img2vid.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+touch list.txt
 for IMGFILE in $@; do
   VIDFILE=${IMGFILE%\.*}.mp4
   VIDFILE=mp4_aljazeera${VIDFILE##data_aljazeera}
   DIRNAME=$(dirname $VIDFILE)
   mkdir -p $DIRNAME
   ffmpeg -loop 1 -i $IMGFILE -s hd720 -c:v mpeg4 -pix_fmt yuv420p $VIDFILE
+  echo $VIDFILE >> list.txt
 done
+
+ffmpeg -f concat -safe 0 -i list.txt -c copy video.mp4
+rm list.txt
