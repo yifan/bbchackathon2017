47769f4518ede73b7aa30fdfc8242cc5b5a15f67
Yifan Zhang <freqyifan@gmail.com>
2017-11-21 18:19:42 +0000

    WIP: subtitle generation

--- img2vid.sh
+++ img2vid.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 touch list.txt
-cat images.txt | while read IMGFILE; do
-  VIDFILE=${IMGFILE%\.*}.mp4
-  WAVFILE=$(dirname $IMGFILE).wav
-  VIDFILE=mp4_aljazeera${VIDFILE##data_aljazeera}
-  DIRNAME=$(dirname $VIDFILE)
-  mkdir -p $DIRNAME
-  ffmpeg -loop 1 -t 1 -i $IMGFILE -s hd720 -c:v mpeg4 -pix_fmt yuv420p -vf scale=1280:720 $VIDFILE
-  if [[ -s $VIDFILE ]]; then
-    echo file \'$VIDFILE\' >> list.txt
-  else
-    echo ffmpeg -loop 1 -t 1 -i $IMGFILE -s hd720 -c:v mpeg4 -pix_fmt yuv420p -vf scale=1280:720 $VIDFILE >> failed.txt
-  fi
+for DIRNAME in $(find data_aljazeera -depth 1 -type d); do
+  WAVFILE=$DIRNAME.wav
+  DURATION=$(soxi -D $WAVFILE)
+  NUMIMGS=$(ls $DIRNAME | wc -l)
+  IMGDUR=$(echo "scale=2;c=$DURATION / $NUMIMGS;if (c > 1.5) { print c; } else { print 1.5; }" | bc -l)
+  CORRECT_DURATION=$(echo "scale=2; $IMGDUR * $NUMIMGS" | bc -l)
+  echo $DIRNAME $IMGDUR $CORRECT_DURATION
+  for IMGFILE in $DIRNAME/*.jpg; do
+    echo "    " $IMGFILE
+    VIDFILE=$DIRNAME/$(basename $IMGFILE .jpg).mp4
+    VIDFILE=mp4_aljazeera${VIDFILE##data_aljazeera}
+    mkdir -p $(dirname $VIDFILE)
+    ffmpeg -y -loop 1 -t $IMGDUR -i $IMGFILE -s hd720 -c:v mpeg4 -pix_fmt yuv420p -vf scale=1280:720 $VIDFILE &> /dev/null
+    if [[ -s $VIDFILE ]]; then
+      echo file \'$VIDFILE\' >> list.txt
+    else
+      echo ffmpeg -loop 1 -t 1 -i $IMGFILE -s hd720 -c:v mpeg4 -pix_fmt yuv420p -vf scale=1280:720 $VIDFILE >> failed.txt
+    fi
+  done
 done
 
-ffmpeg -f concat -i list.txt -c copy video.mp4
+ffmpeg -y -f concat -i list.txt -c copy video.mp4 &> /dev/null
 rm list.txt
---
+++ scripts/subtitle.py
@@ -0,0 +1,29 @@
+import sys
+import codecs
+from pycaption import Caption, CaptionSet, CaptionNode
+from pycaption import SRTWriter
+
+stories = codecs.open('story.txt', 'r', 'utf-8').readlines()
+
+offset = 0.0
+captions = []
+for line in sys.stdin:
+  if line.startswith(' '):
+    continue
+  tokens = line.split()
+  if len(tokens) != 3:
+    continue
+  dirname = tokens[0]
+  index = int(dirname.split('/')[-1])-1
+  duration = float(tokens[2])
+  print duration
+  text=stories[index]
+  cap = Caption(offset, offset+duration, [CaptionNode.create_text(text)])
+  offset += duration
+  captions.append(cap)
+
+caps = CaptionSet({'en': captions})
+
+srt = codecs.open('output.srt', 'w', 'utf-8')
+srt.write(SRTWriter().write(caps))
+srt.close()
